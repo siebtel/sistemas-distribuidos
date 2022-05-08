830077d2dce6e6aab25c135ef8f5a7d6898127b3
siebtel <siebtel.ss@gmail.com>
2022-05-08 12:50:45 -0300

    fix: return name on callback function of FilterTagged

--- pdf2txt/pdf_reader.py
+++ pdf2txt/pdf_reader.py
@@ -4,11 +4,6 @@ import pytesseract
 import sys 
 from pdf2image import convert_from_path 
 import os, os.path
-
-# import pathlib
-# pathlib.Path().resolve()
-# Path of the pdf 
-
 ''' 
 Part #1 : Converting PDF to images 
 '''
@@ -45,49 +40,4 @@ def pdf2image(file_name):
 		# Increment the counter to update filename 
 		image_counter = image_counter + 1
 
-	''' 
-	Part #2 - Recognizing text from the images using OCR 
-	'''
-def image2txt(filename):
-
-	outfile = "./txt_from_pdf/" + file_name + ".txt"
-	# Variable to get count of total number of pages 
-	filelimit = image_counter-1
-
-	# Creating a text file to write the output 
-
-	# Open the file in append mode so that 
-	# All contents of all images are added to the same file 
-	f = open(outfile, "a") 
-
-	# Iterate from 1 to total number of pages 
-	for i in range(1, filelimit + 1): 
-		print("jpg to txt: ", i)
-		# Set filename to recognize text from 
-		# Again, these files will be: 
-		# page_1.jpg 
-		# page_2.jpg 
-		# .... 
-		# page_n.jpg 
-		filename = "./temp_images/page_"+str(i)+".jpg"
-		
-		# Recognize the text as string in image using pytesserct 
-		text = str(((pytesseract.image_to_string(Image.open(filename))))) 
-
-		# The recognized text is stored in variable text 
-		# Any string processing may be applied on text 
-		# Here, basic formatting has been done: 
-		# In many PDFs, at line ending, if a word can't 
-		# be written fully, a 'hyphen' is added. 
-		# The rest of the word is written in the next line 
-		# Eg: This is a sample text this word here GeeksF- 
-		# orGeeks is half on first line, remaining on next. 
-		# To remove this, we replace every '-\n' to ''. 
-		text = text.replace('-\n', '')
-
-		# Finally, write the processed text to the file. 
-		f.write(text)
-
-
-	# Close the file after writing all the text. 
-	f.close() 
+		return ouput_filename
